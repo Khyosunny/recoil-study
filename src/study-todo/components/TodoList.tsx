dbf3ac02b5b4aaf2d4d00d5c8fd8f4af930eb96e
Khyosunny <kimhyo_0218@naver.com>
2022-05-30 23:45:27 +0900

    Selector study / create todo list stats

--- src/study-todo/components/TodoList.tsx
+++ src/study-todo/components/TodoList.tsx
@@ -3,14 +3,15 @@ import { filteredTodoListState } from 'src/study-todo/atoms/todoState';
 import TodoItemCreator from './TodoCreator';
 import TodoItem from './TodoItem';
 import TodoListFilters from './TodoListFilters';
+import TodoListStats from './TodoListStats';
 
 export default function TodoList() {
   const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <>
+      <TodoListStats />
       <TodoListFilters />
-
       <TodoItemCreator />
       {todoList.map((todoItem) => (
         <TodoItem key={todoItem.id} item={todoItem} />
